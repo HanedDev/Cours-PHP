7cfdf123098abfdf553f1fc5e4950b110fa555db
HanedDev <hanedismed@gmail.com>
2022-11-28 10:10:27 +0100

    PHP

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+    require "Courphp/include/header.php";
+?>
+
+<body>
+
+<?php
+
+require "Courphp/include/navbar.php";
+?>
+
+
+<?php
+
+require "Courphp/include/footer.php";
+?>
+    
+</body>
+
+</html>
